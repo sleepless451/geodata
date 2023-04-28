1673212caea8cdf2a4a38a4f746dc17125b19847
sleepless451 <danyliuk.vinchenzo@gmail.com>
2023-04-28 20:16:22 +0300

    completed lab 6,7,8,9

---
+++ lab9/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import rasterio
+import geopandas as gpd
+
+app = Flask(__name__)
+
+file_path = '../dataset2/soil_moisture.tif'
+with rasterio.open(file_path) as src:
+    bbox = src.bounds
+
+@app.route('/get_image_bbox')
+def get_image_bbox():
+    return jsonify({
+        'lat_max': bbox.top,
+        'lat_min': bbox.bottom,
+        'lon_max': bbox.right,
+        'lon_min': bbox.left
+    })
+
+@app.route('/get_moisture_value')
+def get_moisture_value():
+    lat = float(request.args.get('lat'))
+    lon = float(request.args.get('lon'))
+
+    if not (bbox.left <= lon <= bbox.right and bbox.bottom <= lat <= bbox.top):
+        return jsonify({'moisture': 'no data'})
+
+    with rasterio.open(file_path) as src:
+        row, col = src.index(lon, lat)
+        moisture = int(src.read(1, window=((row, row+1), (col, col+1))))
+
+    return jsonify({'moisture': moisture})
+
+if __name__ == '__main__':
+    app.run(debug=True)
